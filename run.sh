3269c66e40c11804ad7dc46514d0c6c905c3ad82
unknown <ray@tayek.com>
2021-05-07 12:08:18 -0700

    moved jenkins-* folders outside of app.

--- run.sh
+++ run.sh
@@ -7,16 +7,16 @@ docker rm -f jenkins-docker
 docker run --name jenkins-docker --rm --detach \
   --privileged --network jenkins --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
-  --volume "jenkins-docker-certs"s:/certs/client \
-  --volume "jenkins-data":/var/jenkins_home \
+  --volume "D:\jenkins\jenkins-docker-certs"s:/certs/client \
+  --volume "D:\jenkins\jenkins-data":/var/jenkins_home \
   docker:dind
 docker build -t myjenkins-blueocean:1.1 .
 docker rm -f jenkins-blueocean
 docker run --name jenkins-blueocean --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
-  --volume "jenkins-data":/var/jenkins_home \
-  --volume "jenkins-docker-certs":/certs/client:ro \
+  --volume "D:\jenkins\jenkins-data":/var/jenkins_home \
+  --volume "D:\jenkins\jenkins-docker-certs":/certs/client:ro \
   --volume "C:\Users\raz":/home \
   --publish 8080:8080 --publish 50000:50000 myjenkins-blueocean:1.1
 
